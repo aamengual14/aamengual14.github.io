dee396f975d59483af6c5766ab5d242be6813e09
Alfredo Amengual <amengual.alfredo@gmail.com>
2016-08-24 14:56:59 -0500

    auto refresh for responsiveness

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 /* Using jQuery */
 (function($) {
 
+  $(window).bind('resize', function(e)
+{
+  if (window.RT) clearTimeout(window.RT);
+  window.RT = setTimeout(function()
+  {
+    this.location.reload(false); /* false to get page from cache */
+  }, 100);
+});
+
     // Init ScrollMagic
     var ctrl = new ScrollMagic.Controller({
         globalSceneOptions: {
